467e816df70295d31065007088035e022d88e51a
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-02-26 08:48:49 +0700

    push project01

--- project01/html-and-php.php
+++ project01/html-and-php.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile - SAGU Ekskul Day 2</title>
+    <title>HTML and PHP</title>
 </head>
 <body>
-    <h1>Sarcy Nenepat</h1>
+    <?php print("Hello SaCode!") ?>
 </body>
 </html>
---
+++ project01/php-and-html.php
@@ -0,0 +1,17 @@
+<?php 
+    print("
+        <html>
+
+            <head>
+                 <title>PHP and HTML</title>
+            </head>
+
+            <body>
+                 <h1>Hello SAGU!</h1>
+            </body>
+            
+        </html>
+        
+        <p>We are learning PHP codes</p>
+    ");
+?>
