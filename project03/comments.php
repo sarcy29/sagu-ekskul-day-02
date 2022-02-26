1c7f523e5f8a7c512d7060b9e51fdee4286c7440
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-02-26 09:32:15 +0700

    push project 03

---
+++ project03/comments.php
@@ -0,0 +1,18 @@
+<?php
+
+    // this is my first single line comment in php
+
+    echo "Single line comment using '//' <br><br>";
+
+    # this is also a 
+    #single line comment
+
+    echo "Single line comment using '#' <br><br>";
+
+    /* 
+    this is 
+    multiple line comment 
+    in php 
+    */
+    echo "Multiple line comment using '/*...*/'"
+?>
